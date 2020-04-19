db9579cccd3525a4d4bd1a60e620dd83c80d47ef
Nitin Gupta <57863681+niftyNitin@users.noreply.github.com>
2020-04-19 17:49:06 +0530

    Add files via upload

---
+++ euler005.java
@@ -0,0 +1,42 @@
+//Project Euler #5: Smallest multiple
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int a0 = 0; a0 < t; a0++){
+            int n = in.nextInt();
+            int product = 1;
+            for(int i = 2; i<=n; i++) {
+                product = product * i / hcf(product, i);
+            }
+            System.out.println(product);
+        }
+    }
+
+    public static int hcf(int a, int b) {
+        int HCF = 1; 
+        if(a < b) {
+            for(int i = 2; i <= a; i++) {
+                if(a % i == 0 && b % i == 0) {
+                HCF = i;
+                }
+            }
+        } else if(b < a) {
+            for(int i = 2; i <= b; i++) {
+                if(a % i == 0 && b % i == 0) {
+                    HCF = i;
+                }
+            }
+        } else {
+            HCF = a;
+        }
+        return HCF;
+    }
+}
