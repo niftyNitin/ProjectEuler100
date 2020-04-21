8f274f619f161b90e5826bc7c9cfe439f50f93da
Nitin Gupta <57863681+niftyNitin@users.noreply.github.com>
2020-04-21 18:42:30 +0530

    Update and rename euler003.java to Euler003.java

--- Euler003.java
+++ Euler003.java
@@ -3,7 +3,7 @@
 import java.util.*;
 import java.math.*;
 
-public class Solution {
+public class Euler003 {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
