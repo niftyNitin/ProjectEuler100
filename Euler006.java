d0232a92020efc0334c7ad38573431755b3bf13a
Nitin Gupta <57863681+niftyNitin@users.noreply.github.com>
2020-04-21 18:40:34 +0530

    Update and rename euler006.java to Euler006.java

--- Euler006.java
+++ Euler006.java
@@ -6,7 +6,7 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Solution {
+public class Euler006 {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
